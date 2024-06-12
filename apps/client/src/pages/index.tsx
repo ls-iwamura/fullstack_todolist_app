0c533ee3d3768a2894b514ebaa609448bc969005
LSW-024\iwamura <iwamura@logical-studio.com>
2024-06-12 09:58:15 +0900

    turbo setting

--- apps/client/src/pages/index.tsx
+++ apps/client/src/pages/index.tsx
@@ -1,10 +1,3 @@
-import {useEffect, useState} from 'react';
-
 export default function Home() {
-  const [a] = useState('a');
-  if (a === 'a')
-    useEffect(() => {
-      console.log(a);
-    }, []);
   return <main></main>;
 }
