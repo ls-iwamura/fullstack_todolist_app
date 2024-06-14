cf7c564da553619a9ffd7551e614124aecb46662
LSW-024\iwamura <iwamura@logical-studio.com>
2024-06-14 10:02:40 +0900

    eslint settings

--- packages/eslint-config/server.js
+++ packages/eslint-config/server.js
@@ -3,14 +3,24 @@ const { rules } = require("eslint-config-prettier");
 module.exports = {
   extends: [
     "eslint:recommended",
+    "plugin:@typescript-eslint/recommended",
     "@hono/eslint-config",
     "plugin:drizzle/recommended",
     "turbo",
     "prettier",
   ],
   parser: "@typescript-eslint/parser",
-  plugins: ["drizzle", 'import'],
+  plugins: ["drizzle", "import"],
   rules: {
+    "@typescript-eslint/no-unused-vars": [
+      "warn",
+      {
+        argsIgnorePattern: "^_",
+        varsIgnorePattern: "^_",
+        caughtErrorsIgnorePattern: "^_",
+        destructuredArrayIgnorePattern: "^_",
+      },
+    ],
     "import/order": [
       "error",
       {
